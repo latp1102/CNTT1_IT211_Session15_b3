39f351ae574182592f83fb86db0fbc6fcc1a8819
La The Phong <latp1102@gmail.com>
2026-06-02 14:05:31 +0700

    Session15_b3

---
+++ src/main/java/org/example/session15_b3/controller/ProductController.java
@@ -0,0 +1,20 @@
+package org.example.session15_b3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @GetMapping("/products")
+    public List<String> getAllProducts() {
+        return Arrays.asList("Laptop Thinkpad", "Mouse Logitech", "Keyboard Akko");
+    }
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to SmartRetail Hub!";
+    }
+
+}
